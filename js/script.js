6bd26fbe9bfddf1745fbbe54b6de52fe697c09eb
matheush212 <matheushhorongoso@gmail.com>
2024-04-25 10:56:57 -0300

    criado tabs e ajustado funcionamento

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function tabControl(home, who_are, portifolio, contact){
+    document.getElementById("home_content").style.setProperty("display", home);
+    document.getElementById("who_are_content").style.setProperty("display", who_are);
+    document.getElementById("portifolio_content").style.setProperty("display", portifolio);
+    document.getElementById("contact_content").style.setProperty("display", contact);
+}
