6ce83b2629756097368e81abd30bdbe7349913d5
matheush212 <matheushhorongoso@gmail.com>
2024-04-27 14:47:24 -0300

    adicionado header fixo

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ function tabControl(home, who_are, portifolio, contact){
     document.getElementById("portifolio_content").style.setProperty("display", portifolio);
     document.getElementById("contact_content").style.setProperty("display", contact);
 }
+
+
+$(function(){
+    var stickyHeaderTop = $('#menu_header').offset().top;
+
+    $(window).scroll(function(){
+        if( $(window).scrollTop() > stickyHeaderTop ) {
+            $('#menu_header').css({position: 'fixed', top: '0px'});
+        } else {
+            $('#menu_header').css({position: 'static', top: '0px'});
+        }
+    });
+});
